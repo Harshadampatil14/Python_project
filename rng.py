0ad261971cfacf2f46db03137347692625d4ea64
Harshadampatil14 <harshadampatil14@gmail.com>
2024-05-10 12:43:13 +1000

    Update rng.py
    test python deployments

--- rng.py
+++ rng.py
@@ -1,11 +1,3 @@
-from random import randint
-print("Hello world!")
-min_number = int(input('Please enter the min number: '))
-max_number = int(input('Please enter the max number: '))
-
-if (max_number < min_number): 
-  print('Invalid input - shutting down...')
-else:
-  rnd_number = randint(min_number, max_number)
-  print(rnd_number)
+ print("Hello world!")
+ 
 
